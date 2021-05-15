21ca6f02d4064ba0ea906ec90a4b46403c6b8598
Bird <39280243+jlambert360@users.noreply.github.com>
2021-05-15 03:43:12 -0400

    Delete netplay files

--- Delete Netplay Only Files.bat
+++ Delete Netplay Only Files.bat
@@ -1,4 +1,10 @@
+del .\KingBird\DOUBLES.txt /Q
+del .\KingBird\DUBBOOST.txt /Q
+del .\KingBird\NETPLAY.txt /Q
+del .\KingBird\NETBOOST.txt /Q
 del .\KingBird\pf\menu3\dnet.cmnu /Q
 rmdir .\KingBird\pf\movie /s /q
+rmdir .\KingBird\pf\stage\stageinfodubs /s /q
 rmdir .\KingBird\pf\sound\netplaylist /s /q
 rmdir .\KingBird\Source\Netplay /s /q
+rmdir .\KingBird\Source\Doubles /s /q
