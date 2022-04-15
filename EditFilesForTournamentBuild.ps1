e1122106146fe42dca47048c9c4c776c909815ab
Bird <39280243+jlambert360@users.noreply.github.com>
2022-04-15 15:42:13 -0400

    Tournament launcher and netplay files

--- EditFilesForTournamentBuild.ps1
+++ EditFilesForTournamentBuild.ps1
@@ -1,10 +1,14 @@
 Write-Host "Enabling Console.asm"
-(Get-Content ".\Build\KingBird\BOOST.txt") -replace '#.include Source/Extras/Console.asm', '.include Source/Extras/Console.asm' | Out-File -encoding ASCII ".\Build\KingBird\BOOST.txt"
-Write-Host "Copying King Build Code Menu"
-copy "./King Build Code Menu/data.cmnu" "./Build/KingBird/pf/menu3/" -Force -Recurse -erroraction 'silentlycontinue'
+(Get-Content ".\TournamentBuild\KingBird\BOOST.txt") -replace '#.include Source/Extras/Console.asm', '.include Source/Extras/Console.asm' | Out-File -encoding ASCII ".\TournamentBuild\KingBird\BOOST.txt"
+
+Write-Host "Enabling Tournament Code Menu"
+(Get-Content ".\TournamentBuild\KingBird\RSBE01.txt") -replace "CodeMenu.asm", "Tourney-CodeMenu.asm" | Out-File -encoding ASCII ".\TournamentBuild\KingBird\RSBE01.txt"
+
 Write-Host "Enabling ALC"
-(Get-Content ".\Build\KingBird\RSBE01.txt") -replace "0417F368 08010100", "0417F368 08010101" | Out-File -encoding ASCII ".\Build\KingBird\RSBE01.txt"
+(Get-Content ".\TournamentBuild\KingBird\RSBE01.txt") -replace "0417F368 08010100", "0417F368 08010101" | Out-File -encoding ASCII ".\TournamentBuild\KingBird\RSBE01.txt"
+
 Write-Host "Setting Default Stagelist"
-(Get-Content ".\Build\KingBird\RSBE01.txt") -replace "0417F36C 00000000", "0417F36C 00000000`r`n* 0417BE74 04A08521 # Brawl stages`r`n* 0417BE70 00020000 # Melee stages" | Out-File -encoding ASCII ".\Build\KingBird\RSBE01.txt"
+(Get-Content ".\TournamentBuild\KingBird\RSBE01.txt") -replace "0417F36C 00000000", "0417F36C 00000000`r`n* 0417BE74 04A08521 # Brawl stages`r`n* 0417BE70 00020000 # Melee stages" | Out-File -encoding ASCII ".\TournamentBuild\KingBird\RSBE01.txt"
+
 Write-Host "Disabling Fudgepop code menu"
-(Get-Content ".\Build\KingBird\RSBE01.txt") -replace '.include Source/Extras/LoadFiles.asm', '!.include Source/Extras/LoadFiles.asm' | Out-File -encoding ASCII ".\Build\KingBird\RSBE01.txt"
+(Get-Content ".\TournamentBuild\KingBird\RSBE01.txt") -replace '.include Source/Extras/LoadFiles.asm', '!.include Source/Extras/LoadFiles.asm' | Out-File -encoding ASCII ".\TournamentBuild\KingBird\RSBE01.txt"
--- Prepare Tournament Build.bat
+++ Prepare Tournament Build.bat
@@ -1,14 +1,24 @@
+rmdir .\TournamentBuild\ /s /q
+
+@echo Copying to Tournament Build Folder
+Xcopy /E /I .\Build .\TournamentBuild
+
 @echo Deleting Netplay Only Files
-del .\Build\KingBird\NETPLAY.txt /Q
-del .\Build\KingBird\NETBOOST.txt /Q
-del .\Build\KingBird\pf\menu3\dnet.cmnu /Q
-rmdir .\Build\KingBird\pf\movie /s /q
-rmdir .\Build\KingBird\pf\sound\netplaylist /s /q
-rmdir .\Build\KingBird\Source\Netplay /s /q
+del .\TournamentBuild\KingBird\NETPLAY.txt /Q
+del .\TournamentBuild\KingBird\NETBOOST.txt /Q
+del .\TournamentBuild\KingBird\TOURNAMENT.txt /Q
+del .\TournamentBuild\KingBird\pf\menu3\dnet.cmnu /Q
+rmdir .\TournamentBuild\KingBird\pf\movie /s /q
+rmdir .\TournamentBuild\KingBird\pf\sound\netplaylist /s /q
+rmdir .\TournamentBuild\KingBird\Source\Netplay /s /q
+
 powershell.exe .\EditFilesForTournamentBuild.ps1
+
 @echo Building Codesets
-".\Build\KingBird\GCTRealMate.exe" -q ".\Build\KingBird\RSBE01.txt"
-".\Build\KingBird\GCTRealMate.exe" -q ".\Build\KingBird\BOOST.txt"
-@echo Zipping Files Please Wait
-powershell.exe .\ZipWiiFiles.ps1
+".\TournamentBuild\KingBird\GCTRealMate.exe" -q ".\TournamentBuild\KingBird\RSBE01.txt"
+".\TournamentBuild\KingBird\GCTRealMate.exe" -q ".\TournamentBuild\KingBird\BOOST.txt"
+
+::@echo Zipping Files Please Wait
+::powershell.exe .\ZipWiiFiles.ps1
+
 @echo Finished!
